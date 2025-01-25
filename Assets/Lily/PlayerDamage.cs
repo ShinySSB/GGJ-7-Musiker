7aae1846dd9bf705f50915973e16ae054429dc35
ShinySSB <a23lilsa@student.his.se>
2025-01-25 12:28:19 +0100

    worked on fishbehavior and spikes tilemap

--- Assets/Lily/PlayerDamage.cs
+++ Assets/Lily/PlayerDamage.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class PlayerDamage : MonoBehaviour
 {
-
+    public UnityEvent onPlayerDeath;
     public void PlayerDeath()
     {
+        onPlayerDeath.Invoke();
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadSceneAsync(currentSceneName);
     }
