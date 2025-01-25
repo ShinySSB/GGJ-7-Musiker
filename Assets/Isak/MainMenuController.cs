e3a2d23ed5305daca400a3f6d563f2e515502306
IsakBerglund <b23isabe@student.his.se>
2025-01-25 14:39:16 +0100

    MainMenuTempSounds

--- Assets/Isak/MainMenuController.cs
+++ Assets/Isak/MainMenuController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using SonityTemplate;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
@@ -12,13 +13,17 @@ using Slider = UnityEngine.UI.Slider;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    
     
     public UnityEvent settings, back;
+    [Space(10)]
     public TMP_Text musicText, sfxText;
     public Slider musicSlider, SFXSlider;
     [Range(0f, 1f)]
     public float initSliderValue;
-
+    [Space(10)]
+    public UnityEvent buttonClick, buttonHover;
     private void Start()
     {
         musicSlider.value = initSliderValue;
@@ -58,6 +63,17 @@ public class MainMenuController : MonoBehaviour
     {
         sfxText.text = SFXSlider.value.ToString("F2"); 
     }
-    
-    
+
+    public void hoverSound()
+    {
+        Debug.Log("hoverSound");
+        buttonHover.Invoke();
+    }
+
+    public void clickSound()
+    {
+        Debug.Log("clickSound");
+        buttonClick.Invoke();
+    }
+
 }
