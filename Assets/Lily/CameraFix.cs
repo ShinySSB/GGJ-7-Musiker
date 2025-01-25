f984b2aa2ebe7bb7a4c204c4e9abe959040334c2
ShinySSB <a23lilsa@student.his.se>
2025-01-25 22:08:52 +0100

    camera fix

--- Assets/Lily/CameraFix.cs
+++ Assets/Lily/CameraFix.cs
@@ -5,12 +5,43 @@ using UnityEngine;
 
 public class CameraFix : MonoBehaviour
 {
+    public bool lockY;
+    public bool lockX;
     public float yLockedValue;
+    public float xLockedValue;
 
     private void LateUpdate()
     {
         var o = gameObject;
         var position = o.transform.position;
-        o.transform.position = new Vector3(position.x, yLockedValue, position.z);
+
+        if (lockY)
+        {
+            o.transform.position = new Vector3(position.x, yLockedValue, position.z);
+        }
+        else if (lockX)
+        {
+            o.transform.position = new Vector3(xLockedValue, position.y, position.z);
+        }
+    }
+
+    public void SwapAxis()
+    {
+        lockY = !lockY;
+        lockX = !lockX;
+
+        if (lockX)
+        {
+            xLockedValue = gameObject.transform.position.x;
+        }
+        else if (lockY)
+        {
+            yLockedValue = gameObject.transform.position.y;
+        }
+    }
+
+    public void UpdateAxisPosition(bool value)
+    {
+        //Do Something
     }
 }
