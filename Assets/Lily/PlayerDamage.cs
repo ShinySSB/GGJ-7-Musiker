2fc431d0c0f8034d2ab0a25d49c38163b085c3bb
ShinySSB <a23lilsa@student.his.se>
2025-01-25 17:47:54 +0100

    death and death anim

--- Assets/Lily/PlayerDamage.cs
+++ Assets/Lily/PlayerDamage.cs
@@ -7,10 +7,22 @@ using UnityEngine.SceneManagement;
 public class PlayerDamage : MonoBehaviour
 {
     public UnityEvent onPlayerDeath;
+    public float waitTime;
     public void PlayerDeath()
     {
         onPlayerDeath.Invoke();
+        StartCoroutine(RespawnPlayer());
+    }
+
+    public IEnumerator RespawnPlayer()
+    {
+        yield return new WaitForSeconds(waitTime);
         Vector3 respawnPos = GetComponent<CheckpointTracker>().GetCheckpoint();
+        /*if (respawnPos. == null)
+        {
+            
+        }*/
+
         gameObject.transform.position = respawnPos;
     }
 }
