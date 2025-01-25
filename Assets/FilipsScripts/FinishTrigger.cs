bd7e957aebaa649b3d7ad911de827646525cb86d
a23robkr <a23robkr@student.his.se>
2025-01-25 20:42:14 +0100

    Half functioning help me guy

--- Assets/FilipsScripts/FinishTrigger.cs
+++ Assets/FilipsScripts/FinishTrigger.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class FinishTrigger : MonoBehaviour
 {
-    public Scene nextScene;
-    
+    public UnityEvent OnEnter;
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        OnEnter.Invoke();
+    }
 }
